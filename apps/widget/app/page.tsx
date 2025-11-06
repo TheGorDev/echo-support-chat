bf5259b92d1a9732a78fb7cd63c40fe53622a972
Eugene_Gor <a0664640583@gmail.com>
2025-11-06 17:52:14 +0200

    09: widget layout

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,26 +1,15 @@
 'use client';
 
-import { useVapi } from '@/modules/widget/hooks/use-vapi';
-import { Button } from '@workspace/ui/components/button';
+import { WidgetView } from '@/modules/widget/ui/views/widget-view';
+import { use } from 'react';
 
-export default function Page() {
+interface Props {
+  searchParams: Promise<{oranizationId: string}>
+}
 
-  const {isSpeaking, isConnected, isConnecting, transcript, startCall, endCall} = useVapi();
-
+export default function Page({searchParams}: Props) {
+  const {oranizationId} = use(searchParams)
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh max-w-md mx-auto w-full">
-      <Button onClick={startCall} variant="success" className="mb-4" disabled={isConnected || isConnecting}>
-        Start Call
-      </Button>
-      <Button onClick={endCall} variant="destructive" className="mt-4" disabled={!isConnected && !isConnecting}>
-        End Call
-      </Button>
-      <div className="mt-6 p-4 border rounded w-full min-h-[100px]">
-        <p>isConnected: {`${isConnected}`}</p>
-        <p>isConnecting: {`${isConnecting}`}</p>
-        <p>isSpeaking: {`${isSpeaking}`}</p>
-        <p>{JSON.stringify(transcript, null, 2)}</p>
-      </div>
-    </div>
+    <WidgetView oranizationId={oranizationId}/>
   )
 }
