29e82a366d47c875db24bd9c19312b1fba21a1ca
Eugene_Gor <a0664640583@gmail.com>
2025-11-06 08:01:21 +0200

    init widget

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,12 +1,10 @@
 import { Button } from "@workspace/ui/components/button"
-import {Input} from "@workspace/ui/components/input"
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Input placeholder="Type something..." />
+        <h1 className="text-2xl font-bold">Hello World WIDGET</h1>
         <Button size="sm">Button</Button>
       </div>
     </div>
