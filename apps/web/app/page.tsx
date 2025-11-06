0a8a1770235c1031d990b5533f611a0e99b7933f
Eugene_Gor <a0664640583@gmail.com>
2025-11-06 11:02:45 +0200

    03: clerk authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Authenticated, Unauthenticated } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 import {useMutation, useQuery} from "convex/react";
 import {api} from "@workspace/backend/_generated/api";
 import {Button} from "@workspace/ui/components/button";
@@ -8,16 +10,26 @@ export default function Page() {
   const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add)
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <p>apps/web</p>
-      <div className="gap-4 max-w-sm w-full mx-auto">
-        <Button onClick={() => addUser()}>
-          Add User
-        </Button>
+    <>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+      <Authenticated>
+        <UserButton />        
+          <div className="gap-4 max-w-sm w-full mx-auto">
+            <Authenticated>
+              <Button onClick={() => addUser()}>
+                Add User
+              </Button>
+            </Authenticated>
+          </div>
+          <div className="max-w-sm w-full mx-auto">
+            {JSON.stringify(users, null, 2)}
+          </div>
+      </Authenticated>
+        <p>apps/web</p>
       </div>
-      <div className="max-w-sm w-full mx-auto">
-        {JSON.stringify(users, null, 2)}
-      </div>
-    </div>
+    </>
   )
 }
